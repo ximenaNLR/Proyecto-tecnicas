70b5aabaf17e0ee91af61bc99550bbf293ef04e8
ximenaNLR <nicolle.ximena06@gmail.com>
2024-05-30 21:58:29 -0500

    Update Menu.java
    puse nombres xd

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,7 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Nicolle Ximena Llanos Rivera
+//Laura Valentina Lopez Garcia
 package menu;
 
 /**
